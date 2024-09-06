d7bcfbfe68f5a9c7e0ddd8cc5c9f12d5e8be6d23
Mononaut <github@monospace.live>
2024-09-06 17:44:56 +0000

    stratum backend config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -310,7 +310,9 @@ class Server {
       priceUpdater.setRatesChangedCallback(websocketHandler.handleNewConversionRates.bind(websocketHandler));
     }
     loadingIndicators.setProgressChangedCallback(websocketHandler.handleLoadingChanged.bind(websocketHandler));
-    stratumApi.connectWebsocket();
+    if (config.STRATUM.ENABLED) {
+      stratumApi.connectWebsocket();
+    }
   }
 
   setUpHttpApiRoutes(): void {
